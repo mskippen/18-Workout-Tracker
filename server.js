b20a96f8e1ddcc5e70782d54cd07570a712051e6
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-07-03 21:18:14 +1000

    fixed warning on mongodb connection

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(morgan("dev"))
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/workout_db"
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
 
 app.listen(PORT, () => console.log("app running on " + PORT))
