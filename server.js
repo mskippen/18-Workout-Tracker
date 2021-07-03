6295e4d16971f8087e042148aeb16457dfb8ad51
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-07-03 21:07:37 +1000

    Created app boiler plate and README

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(morgan("dev"))
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/workout_db"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+
+app.listen(PORT, () => console.log("app running on " + PORT))
